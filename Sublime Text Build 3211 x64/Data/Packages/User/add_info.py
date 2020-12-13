ccb250e6df06ab110601de620af0001f6e293b60
SD-20201212HOBB\Administrator <wuzezhang@yeah.net>
2020-12-13 21:34:01 +0800

    add time

--- Sublime Text Build 3211 x64/Data/Packages/User/add_info.py
+++ Sublime Text Build 3211 x64/Data/Packages/User/add_info.py
@@ -4,12 +4,8 @@ class AddInfoCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command("insert_snippet",
             {
-                "contents": "--[[""\n"
-                "简介：${1:xxx}""\n"
-                "Author：wzz""\n"
-                "Date："  "%s"  %datetime.datetime.now().strftime("%Y-%m-%d") +"\n"
-                "--]]"
+                "contents": "%s"%datetime.datetime.now().strftime("%Y-%m-%d")
             }
         )
-        sublime.error_message("hello world")
+        sublime.error_message("Add Time")
 
