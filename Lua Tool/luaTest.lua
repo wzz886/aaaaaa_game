0836c2179180151228822cb845e100cd574c2966
SD-20201212HOBB\Administrator <wuzezhang@yeah.net>
2020-12-15 00:16:40 +0800

    add log

--- Lua Tool/luaTest.lua
+++ Lua Tool/luaTest.lua
@@ -1,1 +1,24 @@
-2020-12-13
+local inspect = require "inspect"
+
+local function auto_join(...)
+    local s = ""
+    local t = {...}
+    local n = select('#', ...)
+    for i = 1, n do
+        local v = t[i]
+        if type(v) == "table" then
+            s = s .. inspect(v, {kfilter = {"class", "<metatable>"}}) .. ",\n"
+        else
+            s = s .. tostring(v) .. "\n"
+        end
+    end
+    return s
+end
+
+
+local tab1 = {33, 44}
+local tab = {1, 2, ["tab"] = "tab", ["1"] = 11, {1, 2, 3, tab1}}
+
+print(inspect(tab))
+
+
